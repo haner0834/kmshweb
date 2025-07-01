08cf6cd8433cff332b96077af0b74e1d8f4693b7
Andy <linandy40804@gmail.com>
2025-07-01 20:05:33 +0800

    [feat] update Exam and Subject models to allow nullable score fields; add score table parsing services

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,12 +1,7 @@
-import express, { response } from 'express';
+import express from 'express';
 import cors from 'cors';
 import authRouter from "./routers/auth.router"
 import cookieParser from 'cookie-parser';
-// import { loginOldSeniorSystem, getExamScoreTable } from './services/crawler.senior.service';
-import { Request, Response } from 'express';
-import * as seniorSystem from './services/crawler.senior.service';
-import { SeniorLoginData } from './types/crawler.senior.types';
-import { parseProfile } from './services/parser.senior.service';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,21 +11,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRouter);
-app.use("/abc/def", async (req: Request, res: Response) => {
-    const id = process.env.ID || "?"
-    const password = process.env.PASSWORD || "?"
-
-    const credential: SeniorLoginData = { sid: id, password }
-    const cookie = await seniorSystem.loginAndGetCookie(credential)
-    console.log(cookie)
-
-    seniorSystem.initializeSession(cookie)
-
-    const profileContent = await seniorSystem.getStudentProfile(cookie)
-    console.log(profileContent)
-
-    console.log("Parsed content:", parseProfile(profileContent))
-})
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
