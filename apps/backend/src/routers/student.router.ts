f674e017249b540823878472b487b3cb5009a794
Andy <linandy40804@gmail.com>
2025-07-03 14:52:36 +0800

    [feat] add student router for profile and semester endpoints

---
+++ apps/backend/src/routers/student.router.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { protect } from "../middleware/auth.middleware";
+import { getStudentProfileAndUpdateHandler, getSemestersHandler, getCurrentSemesterHandler, getExamByNameInCurrentSemesterHandler } from "../controllers/student.controller";
+
+const router = Router()
+
+router.get("/profile", protect, getStudentProfileAndUpdateHandler)
+router.get("/profile/update", protect, getStudentProfileAndUpdateHandler)
+
+router.get("/semesters", protect, getSemestersHandler)
+router.get("/semesters/current", protect, getCurrentSemesterHandler)
+router.get("/semesters/current/exam", protect, getExamByNameInCurrentSemesterHandler)
+
+export default router
--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import authRouter from "./routers/auth.router"
+import studentRouter from "./routers/student.router"
 import cookieParser from 'cookie-parser';
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRouter);
+app.use('api/student', studentRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
