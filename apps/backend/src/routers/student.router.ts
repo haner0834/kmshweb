af897c279189a73012392fb749e50c6ad092eabe
Andy <linandy40804@gmail.com>
2025-07-05 03:05:40 +0800

    [fix] update profile route to use correct handler for student profile retrieval

--- apps/backend/src/routers/student.router.ts
+++ apps/backend/src/routers/student.router.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { protect } from "../middleware/auth.middleware";
-import { getStudentProfileAndUpdateHandler, getSemestersHandler, getCurrentSemesterHandler, getExamByNameInCurrentSemesterHandler, getCurrentSemesterAndUpdateHandler } from "../controllers/student.controller";
+import { getStudentProfileAndUpdateHandler, getSemestersHandler, getCurrentSemesterHandler, getExamByNameInCurrentSemesterHandler, getCurrentSemesterAndUpdateHandler, getStudentProfileHandler } from "../controllers/student.controller";
 
 const router = Router()
 
-router.get("/profile", protect, getStudentProfileAndUpdateHandler)
+router.get("/profile", protect, getStudentProfileHandler)
 router.get("/profile/update", protect, getStudentProfileAndUpdateHandler)
 
 router.get("/semesters", protect, getSemestersHandler)
