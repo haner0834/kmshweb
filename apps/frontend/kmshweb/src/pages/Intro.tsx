4468fba8f1933ee2fc48fd63e0c97252d3168c8b
Andy <linandy40804@gmail.com>
2025-07-27 02:03:36 +0800

    [feat] add navbar button logic to Intro page

--- apps/frontend/kmshweb/src/pages/Intro.tsx
+++ apps/frontend/kmshweb/src/pages/Intro.tsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+import type { NavbarButtonType } from "../widgets/Navbar";
+import { useNavbarButtons } from "../widgets/NavbarButtonsContext";
+
 const Intro = () => {
+  const { setNavbarButtonsByType } = useNavbarButtons();
+  useEffect(() => {
+    const buttons: NavbarButtonType[] = ["logo", "themeToggle"];
+    if (!localStorage.getItem("isLoggedIn")) buttons.push("login");
+
+    setNavbarButtonsByType(buttons);
+  }, []);
+
   return (
     // TODO: Complete intro page
     <div className="w-screen flex h-screen items-center justify-center">
