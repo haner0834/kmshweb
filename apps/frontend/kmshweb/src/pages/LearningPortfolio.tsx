ec0f0567bfc678a27aaf7e32dd468068b15e572d
Andy <linandy40804@gmail.com>
2025-09-03 01:14:12 +0800

    [feat] add alert at the top for linking to upcoming rating

--- apps/frontend/kmshweb/src/pages/LearningPortfolio.tsx
+++ apps/frontend/kmshweb/src/pages/LearningPortfolio.tsx
@@ -19,25 +19,29 @@ const LearningPortfolio = () => {
     setNavbarTitle("學習歷程");
   });
   return (
-    <div className="min-h-screen bg-base-100 pt-18">
-      <div role="alert" className="alert">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          className="stroke-info h-6 w-6 shrink-0"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-          ></path>
-        </svg>
-        <span>12 unread messages. Tap to see.</span>
-      </div>
-
+    <div className="min-h-screen bg-base-300 pt-18">
       <ul className="pt-4 mx-auto w-full max-w-xl">
+        <a
+          href="/upcoming/learningportfolio"
+          role="alert"
+          className="alert mx-4"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="stroke-info h-6 w-6 shrink-0"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+          <span>即將推出——點此查看更多</span>
+        </a>
+
         {Object.entries(learningPortfolioLinks).map(([sectionTitle, links]) => (
           <div key={sectionTitle}>
             <SectionTitle title={sectionTitle} />
